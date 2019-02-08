732e3ec721392393726397e4e21709b229a58444
AlexCsanyi <alex.csanyi@gmail.com>
2019-02-08 21:41:36 +0000

    added animations to gallery nav @hover

--- function.js
+++ function.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     let navLinks = document.querySelectorAll('nav a, .social a');
 
+    // Navigation Links hover
     navLinks.forEach((navLink) => {
         navLink.addEventListener('mouseenter', () => {
             let letter = navLink.querySelectorAll('.letter');
@@ -24,5 +25,31 @@ document.addEventListener('DOMContentLoaded', () => {
             }) 
         })
     })
-})
 
+    // Gallery nav hover
+    let galleryLinks = document.querySelectorAll('.gallery-nav div');
+
+    galleryLinks.forEach((galleryLink) => {
+        galleryLink.addEventListener('mouseenter', () => {
+            anime.remove(galleryLink);
+            anime({
+                targets: galleryLink,
+                scale: 1.1,
+                translateX: -5,
+                translateY: -5,
+                easing: 'easeOutExpo',
+            })
+        })
+
+        galleryLink.addEventListener('mouseleave', () => {
+            anime.remove(galleryLink);
+            anime({
+                targets: galleryLink,
+                scale: 1,
+                translateX: 0,
+                translateY: 0,
+                easing: 'easeOutExpo',
+            })
+        })
+    })
+})
