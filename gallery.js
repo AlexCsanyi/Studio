042b892ab39197742fb549db49c4ac94cb7828db
AlexCsanyi <alex.csanyi@gmail.com>
2019-02-09 19:52:25 +0000

    added progress bar animation to sub pages as well

--- gallery.js
+++ gallery.js
@@ -1,27 +1,36 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// next and prev controls
+// next and prev controls pictures
 
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
+function currentDiv(n) {
+    showSlides(slideIndex = n);
+}
+
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("demo");
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" current", "");
+    }
     slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " current";
 }
 
 let numberIndex = 1;
 showNumbers(numberIndex);
 
-// next and prev controls
+// next and prev controls numbers
 
 function plusNumbers(n) {
     showNumbers(numberIndex += n);
