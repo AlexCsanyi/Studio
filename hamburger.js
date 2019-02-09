205b8808b864237416c9886293f5902440321340
AlexCsanyi <alex.csanyi@gmail.com>
2019-02-09 23:19:01 +0000

    added hamburger menu _ simple _ no animation

---
+++ hamburger.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("navigation").style.visibility = "visible";
+    document.getElementById("collapse-menu").style.visibility = "collapse"
+}
+  
+function closeNav() {
+    document.getElementById("navigation").style.visibility = "collapse";
+    document.getElementById("collapse-menu").style.visibility = "visible"
+}
